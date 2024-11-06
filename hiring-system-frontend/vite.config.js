be2e6b9d82e56f2338145c64df5a010c23399e78
pauloRoberto00 <paulorobertoov@gmail.com>
2024-11-06 17:50:49 -0300

    s

--- hiring-system-frontend/vite.config.js
+++ hiring-system-frontend/vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const apiUrl = process.env.VITE_API_URL || 'http://localhost:5000';
+
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -8,7 +10,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: import.meta.env.VITE_API_URL,
+        target: apiUrl,
         changeOrigin: true
       },
     },
