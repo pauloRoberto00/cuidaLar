61ddd50499a3f01061d059d5050dd226806e2257
pauloRoberto00 <paulorobertoov@gmail.com>
2024-11-06 02:41:44 -0300

    sim

--- hiring-system-frontend/vite.config.js
+++ hiring-system-frontend/vite.config.js
@@ -6,11 +6,11 @@ export default defineConfig({
   server: {
     host: true,
     port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true
-      },
-    },
+    // proxy: {
+    //   '/api': {
+    //     target: 'http://localhost:5000',
+    //     changeOrigin: true
+    //   },
+    // },
   },
 });
