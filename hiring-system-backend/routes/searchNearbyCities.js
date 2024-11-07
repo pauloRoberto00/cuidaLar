3177a2416ce68e6ab6fd1211f4025cbc91a98c59
pauloRoberto00 <paulorobertoov@gmail.com>
2024-11-07 01:08:29 -0300

    pp

--- hiring-system-backend/routes/searchNearbyCities.js
+++ hiring-system-backend/routes/searchNearbyCities.js
@@ -1,12 +1,13 @@
+import axios from 'axios';
 import { Router } from 'express';
 const router = Router();
 
 router.get('/', async (req, res) => {
-  const { lat, lng, radius, maxRows, username } = req.params;
+  const { lat, lng, radius, maxRows, username } = req.query;
   const url = `http://api.geonames.org/findNearbyPlaceNameJSON?lat=${lat}&lng=${lng}&radius=${radius}&maxRows=${maxRows}&username=${username}`;
 
   try {
-    const response = await fetch(url);
+    const response = await axios.get(url);
     res.json(response.data);
   } catch (error) {
     console.error(error);
