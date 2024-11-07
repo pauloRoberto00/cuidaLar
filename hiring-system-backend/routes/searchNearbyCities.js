095b044742d4e3238492acd676b2c3d4071261ad
pauloRoberto00 <paulorobertoov@gmail.com>
2024-11-07 14:44:22 -0300

    pp

--- hiring-system-backend/routes/searchNearbyCities.js
+++ hiring-system-backend/routes/searchNearbyCities.js
@@ -8,6 +8,7 @@ router.get('/', async (req, res) => {
 
   try {
     const response = await axios.get(url);
+    console.log(response.data)
     res.json(response.data);
   } catch (error) {
     console.error(error);
