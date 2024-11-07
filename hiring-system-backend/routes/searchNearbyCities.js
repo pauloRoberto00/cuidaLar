61864f068e2d06abd942a21a91962aee7c256849
pauloRoberto00 <paulorobertoov@gmail.com>
2024-11-07 01:01:10 -0300

    pp

--- hiring-system-backend/routes/searchNearbyCities.js
+++ hiring-system-backend/routes/searchNearbyCities.js
@@ -5,13 +5,13 @@ router.get('/', async (req, res) => {
   const { lat, lng, radius, maxRows, username } = req.params;
   const url = `http://api.geonames.org/findNearbyPlaceNameJSON?lat=${lat}&lng=${lng}&radius=${radius}&maxRows=${maxRows}&username=${username}`;
 
-  await fetch(url)
-  .then(response => res.json(response.data)
-  .catch(error => {
-      console.log(body);
-      console.error(error);
-      res.status(500).json({ error: 'Erro ao buscar dados do GeoNames' });
-  })); 
+  try {
+    const response = await fetch(url);
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Erro ao buscar dados do GeoNames' });
+  }
 });
 
 export default router;
