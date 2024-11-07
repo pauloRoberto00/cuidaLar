1f4c89c22199e60e2ca4bc343d16cbb2a9c24e37
pauloRoberto00 <paulorobertoov@gmail.com>
2024-11-07 14:18:54 -0300

    pp

--- hiring-system-frontend/src/components/searchNearbyCities.jsx
+++ hiring-system-frontend/src/components/searchNearbyCities.jsx
@@ -15,6 +15,7 @@ const searchNearbyCities = async (lat, lng) => {
         maxRows: 350,
       }
     });
+    console.log(response.data);
     return response.data.geonames ? response.data.geonames.map(city => city.name) : [];
   } catch (error) {
     console.error('Error fetching nearby cities:', error);
