cfaee49a0c90c13e749c3aa319d4a5b659d26d7a
pauloRoberto00 <paulorobertoov@gmail.com>
2024-11-07 00:03:28 -0300

    pp

--- hiring-system-frontend/src/components/searchNearbyCities.jsx
+++ hiring-system-frontend/src/components/searchNearbyCities.jsx
@@ -3,17 +3,18 @@ import axios from 'axios';
 const searchNearbyCities = async (lat, lng) => {
   // const url = `https//api.geonames.org/findNearbyPlaceNameJSON?lat=${lat}&lng=${lon}&radius=${radius}&maxRows=${maxRows}&username=${username}`;
   const apiUrl = import.meta.env.VITE_API_URL;
-  const data = {
-    lat: lat,
-    lng: lng,
-    username: 'hasuki_',
-    radius: 150,
-    maxRows: 300,
-  };
   const url = `${apiUrl}/searchNearbyCities`;
 
   try {
-    const response = await axios.get(url, data);
+    const response = await axios.get(url, {
+      params: {
+        lat: lat,
+        lng: lng,
+        username: 'hasuki_',
+        radius: 150,
+        maxRows: 300,
+      }
+    });
     return response.data.geonames ? response.data.geonames.map(city => city.name) : [];
   } catch (error) {
     console.error('Error fetching nearby cities:', error.message);
